d5ba30a024cbd8cbd47f70dfbd76546d7ef583f9
cblair1994 <blair.chelsea@gmail.com>
2017-03-20 17:04:45 -0700

    Chapter6

---
+++ Chapter6/contact.js
@@ -0,0 +1,62 @@
+/*
+	CIS166AA: Book Blog
+	Author: Chelsea Blair
+	Date: 2/25/2017
+*/
+
+function insertPlaceholders()
+	  {
+         if (!Modernizr.input.placeholder)
+		 {
+            document.getElementById("nameinput").value = "first and last name";
+            document.getElementById("emailinput").value = "address@example.com";
+            document.getElementById("bookinput").value = "Twilight by Stephenie Meyers";
+         }
+      }
+      if (window.addEventListener)
+	  {
+         window.addEventListener("load", insertPlaceholders, false);
+      } 
+	  else if (window.attachEvent)
+	  {
+         window.attachEvent("onload", insertPlaceholders);
+      } 
+
+/*
+	this function is used to make sure that the user had
+	filled out the form.
+	if not it will tell the user what they missed.
+*/	  
+function validateContactInfo()
+{	
+	var userName;
+	var userEmail;
+	var userBook;
+	var formValidity = true;
+	userName = document.getElementById("nameinput").value;
+	userEmail = document.getElementById("emailinput").value;
+	userBook = document.getElementById("bookinput").value;
+	try
+	{
+		if(userName.length == 0)
+		{
+			throw alert("Please enter your name.");
+			formValidity = false;
+		}
+		if(userEmail.length == 0)
+		{
+			throw alert("Please enter your email.");
+			formValidity = false;
+		}
+		if(userBook.length == 0)
+		{
+			throw alert("Please enter your book. ");
+			formValidity = false;
+		}		
+	}
+	catch(error)
+	{
+		document.getElementById("message").innerHTML = "warning: " + error.message;
+		formValidity = false;
+	}
+}
